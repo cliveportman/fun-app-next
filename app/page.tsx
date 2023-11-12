19aef280eb6364dc29ccdeb335fbbd71ecb53198
cliveportman <clive@cliveportman.co.uk>
2023-11-12 22:49:15 +0000

    tailwind, open sans, distance and time formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(duration)
+
 async function getData() {
   const res = await fetch('https://fun-api-next.vercel.app/api/activity')
   // The return value is *not* serialized
@@ -6,7 +10,7 @@ async function getData() {
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
-  }
+  }  
  
   return res.json()
 }
@@ -15,12 +19,13 @@ export default async function Page() {
   const activity = await getData()
  
   return (
-    <main>
-      <ul>
-        <li>{activity.name }</li>
-        <li>{ activity.distance }</li>
-        <li>{ activity.duration }</li>
+    <main className="p-6">
+      <ul className="text-yellow-200 text-5xl font-bold leading-tight">
+        <li className="font-bold">{activity.name }</li>
+        <li>{ (activity.distance / 1000).toLocaleString('en-GB', { minimumFractionDigits: 1, maximumFractionDigits: 1 }) } km</li>
+        <li>{ dayjs.duration(activity.duration, 'seconds').format('H [hrs] m [min]') }</li>
       </ul>
     </main>
   );
+
 }
